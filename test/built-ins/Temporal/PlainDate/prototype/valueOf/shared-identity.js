8f83ba04964230df9c5393a5ec44573b6701d578
Philip Chimento <pchimento@igalia.com>
2024-06-09 18:47:57 +0200

    Temporal: Add tests for shared identity of valueOf methods

---
+++ test/built-ins/Temporal/PlainDate/prototype/valueOf/shared-identity.js
@@ -0,0 +1,17 @@
+// Copyright (C) 2024 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.plaindate.prototype.valueof
+description: Same function object as other Temporal valueOf methods
+features: [Temporal]
+---*/
+
+const valueOf = Temporal.PlainDate.prototype.valueOf;
+assert.sameValue(valueOf, Temporal.Duration.prototype.valueOf);
+assert.sameValue(valueOf, Temporal.Instant.prototype.valueOf);
+assert.sameValue(valueOf, Temporal.PlainDateTime.prototype.valueOf);
+assert.sameValue(valueOf, Temporal.PlainMonthDay.prototype.valueOf);
+assert.sameValue(valueOf, Temporal.PlainTime.prototype.valueOf);
+assert.sameValue(valueOf, Temporal.PlainYearMonth.prototype.valueOf);
+assert.sameValue(valueOf, Temporal.ZonedDateTime.prototype.valueOf);
