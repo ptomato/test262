05b55781ff959b41505520dd51d848a87436e7bc
Philip Chimento <pchimento@igalia.com>
2025-02-11 17:21:34 -0800

    proof of concept ChakraCore implementation tests importer

---
+++ harness/chakracore/adaptor.js
@@ -0,0 +1,145 @@
+// Copyright (C) 2024 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+description: Adaptor for ChakraCore implementation tests
+defines: [WScript, console, print, CollectGarbage, chakraCoreAdaptor]
+---*/
+// If verifyTest() is called with an argument, then it's necessary to include
+// compareArray.js in the test.
+
+var ArrayPrototypeJoin = Array.prototype.join;
+var ArrayPrototypeMap = Array.prototype.map;
+var ReflectApply = Reflect.apply;
+var StringCtor = String;
+
+let log = '';
+
+function __print(...args) {
+  const argStrings = ReflectApply(ArrayPrototypeMap, args, [StringCtor]);
+  const logMsg = ReflectApply(ArrayPrototypeJoin, argStrings, [" "]);
+  log += logMsg + "\n";
+}
+
+var print = __print;  // may be overwritten and hoisted in test files
+
+const knownScriptFiles = [
+  "..\\UnitTestFramework\\UnitTestFramework.js",
+  "../UnitTestFramework/UnitTestFramework.js",
+  ".\\memset_tester.js",
+]
+
+var WScript = {
+  Arguments: [],
+  Echo: __print,
+  LoadModule(script, context = "self", filename) {
+    if (context !== "self")
+      throw new Test262Error(`WScript.LoadModule: context '${context}' not implemented`);
+    throw new Test262Error("WScript.LoadModule: not implemented");
+  },
+  LoadScript(script, context = "self", filename) {
+    if (context !== "self")
+      throw new Test262Error(`WScript.LoadScript: context '${context}' not implemented`);
+    void filename;
+    const realm262 = $262.createRealm();
+    realm262.evalScript(script);
+    return realm262.global;
+  },
+  LoadScriptFile(path) {
+    if (!(knownScriptFiles.includes(path)))
+      throw new Test262Error(`WScript.LoadScriptFile: Unhandled script file path ${path}`);
+  },
+  Platform: {
+    BUILD_TYPE: "Release",
+  },
+};
+
+var console = {
+  log: __print,
+};
+
+function CollectGarbage() {
+  $262.gc();
+}
+
+const excMessageMatcher = /^(?:Range|Reference|Syntax|Type)Error\s*:/;
+const excMessageWorkaroundMapping = new Map([
+  ['Array length must be assigned a finite positive integer', function (msg) {
+    try {
+      [].length = -1;
+    } catch (e) {
+      return msg === e.message;
+    }
+    throw new Test262Error("Could not determine workaround exception message");
+  }],
+
+  ['Function expected', function (msg) {
+    try {
+      const foo = 777;
+      foo();
+    } catch (e) {
+      const escaped = e.message.replace(/[$-\/?[-^{|}]/g, '\\$&')
+        .replace(/foo|777/g, '.*')
+      const matcher = new RegExp('^' + escaped + '$');
+      return matcher.test(msg);
+    }
+    throw new Test262Error("Could not determine workaround exception message");
+  }],
+]);
+function fuzzyMatchExceptionMessages(actual, expected) {
+  // Handle the easy case where the message is prefixed with the type of the
+  // error, e.g. "TypeError: Function expected". Just ignore everything after
+  // the colon.
+  const match = excMessageMatcher.exec(actual);
+  if (match && expected.startsWith(match[0]))
+    return true;
+
+  if (excMessageWorkaroundMapping.get(expected)?.(actual))
+    return true;
+
+  return false;
+}
+
+var chakraCoreAdaptor = {
+  verifyTest(expectedOutput) {
+    const logLines = log.split('\n');
+    if (expectedOutput) {
+      const expectedLines = expectedOutput.split('\n');
+
+      if (log !== expectedOutput) {
+        // Heuristic: try to ignore differences in exception messages
+
+        const zipped = logLines.map((line, ix) => ([line, expectedLines[ix]]));
+        const matches = zipped.map(([actual, expected], ix) => {
+          if (actual === expected)
+            return true;
+          if (fuzzyMatchExceptionMessages(actual, expected)) {
+            // Overwrite the actual line with the expected line so if there are
+            // still other differences, it doesn't clutter the output
+            logLines[ix] = expectedLines[ix];
+            return true;
+          }
+          return false;
+        });
+        if (matches.every(Boolean))
+          return;
+      }
+
+      // The following prevents actual and expected from being iterated and evaluated
+      // more than once unless absolutely necessary.
+      const format = compareArray.format;
+      if (!compareArray(logLines, expectedLines)) {
+        throw new Test262Error(
+          `Test log did not match expected log. Expected: ${format(expectedLines)} Actual: ${format(logLines)}`
+        );
+      }
+
+      return;
+    }
+
+    // log should be empty or contain only lines with case-insensitive 'PASS' or
+    // 'Passed'
+    if (log !== "" &&
+      !logLines.every(l => ["", "pass", "passed"].includes(l.toLowerCase())))
+      throw new Test262Error(`Expected test to log nothing or pass, but got ${log}`);
+  },
+};
---
+++ tools/import/chakracore.mjs
@@ -0,0 +1,235 @@
+import { strict as assert } from 'node:assert';
+import dedent from 'dedent';
+import fs from 'node:fs';
+import { mkdirp } from 'mkdirp';
+import XML from '@rgrove/parse-xml';
+import path from 'node:path';
+import url from 'node:url';
+import util from 'node:util';
+
+const knownFailures = {
+};
+
+// This script lives in tools/import/
+const test262Dir = path.resolve(url.fileURLToPath(import.meta.url), '..', '..', '..');
+
+const argsConfig = {
+  options: {
+    chakraCoreDir: {
+      type: 'string',
+      short: 'c',
+      required: true,
+      description: 'Path to the ChakraCore git checkout',
+    },
+    help: {
+      type: 'boolean',
+      short: 'h',
+      description: 'Show this message',
+    },
+  },
+};
+const { values: { chakraCoreDir, help } } = util.parseArgs(argsConfig);
+
+if (help) {
+  console.log('ChakraCore test importer');
+  for (let option in argsConfig.options) {
+    const {required, description} = argsConfig.options[option];
+    console.log(`  --${option.padEnd(15)} - ${description} ${required ? '(required)' : ''}`);
+  }
+  process.exit(0);
+}
+
+if (!chakraCoreDir) {
+  console.log('Provide the path of the ChakraCore checkout with --chakraCoreDir');
+  process.exit(1);
+}
+
+const UTF8 = { encoding: 'utf-8' };
+
+function expectOneChild(xmlNode, childName) {
+  const childElements = xmlNode.children.filter(node => node instanceof XML.XmlElement);
+  assert.equal(childElements.length, 1, `Expected only one child of <${xmlNode.name}>`);
+  assert.equal(childElements[0].name, childName, `Expected <${childName}> child of <${xmlNode.name}>`);
+  return childElements[0];
+}
+
+function* iterateChildren(xmlNode) {
+  for (const childElement of xmlNode.children) {
+    if (!(childElement instanceof XML.XmlElement))
+      continue;
+    yield childElement;
+  }
+}
+
+function* expectChildren(xmlNode, childName) {
+  for (const childElement of iterateChildren(xmlNode)) {
+    assert.equal(childElement.name, childName, `Expected only <${childName}> children of <${xmlNode.name}>`);
+    yield childElement;
+  }
+}
+
+function expectText(xmlNode) {
+  const childText = xmlNode.children.filter(node => node instanceof XML.XmlText);
+  assert.equal(childText.length, 1, `Expected only text inside <${xmlNode.name}>`);
+  return childText[0].text;
+}
+
+function caseInsensitivePath(folder, leafname) {
+  const leaves = fs.readdirSync(folder, UTF8);
+  const realCasedLeaf = leaves.find((l) => l.toLowerCase() === leafname.toLowerCase());
+  return path.join(folder, realCasedLeaf ?? leafname);
+}
+
+const testDirsXml = fs.readFileSync(path.join(chakraCoreDir, 'test', 'rlexedirs.xml'), UTF8);
+const testDirs = XML.parseXml(testDirsXml);
+
+const dirsToProcess = [];
+const regressExeElement = expectOneChild(testDirs, 'regress-exe');
+for (const dirElement of expectChildren(regressExeElement, 'dir')) {
+  const defaultElement = expectOneChild(dirElement, 'default');
+  const dirInfo = {};
+  for (const directiveElement of iterateChildren(defaultElement)) {
+    switch (directiveElement.name) {
+      case 'files':
+        dirInfo.dir = expectText(directiveElement);
+        break;
+      case 'tags':
+        const tags = expectText(directiveElement).split(',');
+        if (tags.includes('exclude_noicu'))
+          dirInfo.intl402 = true;
+        if (tags.includes('require_wasm'))
+          dirInfo.wasm = true;
+        break;
+      default:
+        throw new Error(`Unexpected <${directiveElement.name}> child of <default>`);
+    }
+  }
+  assert('dir' in dirInfo, 'Expected <files> child of <default>');
+  dirsToProcess.push(dirInfo);
+}
+
+const extraIncludes = ['UnitTestFramework.js', 'memset_tester.js'];
+
+for (const { dir, intl402 = false } of dirsToProcess) {
+  const skipDirs = [
+    '262',
+    'ConfigParsing',
+    'UnitTestFramework',
+  ];
+  if (skipDirs.includes(dir))
+    continue;
+
+  const targetDir = path.join(test262Dir, 'test', 'staging', 'ChakraCore', dir);
+  mkdirp.sync(targetDir);
+
+  const testsXml = fs.readFileSync(path.join(chakraCoreDir, 'test', dir, 'rlexe.xml'), UTF8);
+  const tests = XML.parseXml(testsXml)
+  const regressExeElement = expectOneChild(tests, 'regress-exe');
+  for (const testElement of expectChildren(regressExeElement, 'test')) {
+    const defaultElement = expectOneChild(testElement, 'default');
+    const testInfo = {};
+    let skip = false;
+    for (const directiveElement of iterateChildren(defaultElement)) {
+      switch (directiveElement.name) {
+        case 'files':
+          testInfo.file = expectText(directiveElement);
+          if (knownFailures[dir]?.includes(testInfo.file))
+            skip = true;
+          break;
+        case 'baseline':
+          testInfo.expectedOutput = expectText(directiveElement);
+          break;
+        case 'compile-flags':
+          const flags = expectText(directiveElement).split(' ');
+          const start = flags.indexOf('-args');
+          if (start >= 0) {
+            const end = flags.indexOf('-endargs');
+            testInfo.wscriptArgs = flags.slice(start + 1, end);
+          }
+
+          // Tests that rely on printing ChakraCore test traces will not work
+          if (flags.some((flag) => flag.startsWith('-testtrace:')))
+            skip = true;
+
+          break;
+        case 'tags':
+        case 'timeout':
+          break; // ignore
+        default:
+          throw new Error(`Unexpected <${directiveElement.name}> child of <default>`);
+      }
+    }
+    assert('file' in testInfo, 'Expected <files> child of <default>');
+
+    if (skip)
+      continue;
+
+    const testsFolder = path.join(chakraCoreDir, 'test', dir);
+    const testFile = caseInsensitivePath(testsFolder, testInfo.file);
+    let expectedOutput;
+
+    // dos2unix line endings
+    let testContents = fs.readFileSync(testFile, UTF8).replace(/\r/g, '');
+    if (testInfo.expectedOutput) {
+      const expectedOutputFile = caseInsensitivePath(testsFolder, testInfo.expectedOutput);
+      expectedOutput = fs.readFileSync(expectedOutputFile, UTF8).replace(/\r/g, '');
+    }
+
+    const separatorLine = /^\/\/-+\n/d;
+    const copyrightLine = /^\/\/ Copyright.*\n/d;
+    const licenseLine = /^\/\/ Licensed under the (.*?) license.*\n/d;
+    const copyrightLines = [];
+    let license;
+    // eat header separator
+    let separatorMatch = separatorLine.exec(testContents);
+    if (separatorMatch)
+      testContents = testContents.slice(separatorMatch.indices[0][1]);
+    let copyrightMatch;
+    while ((copyrightMatch = copyrightLine.exec(testContents))) {
+      copyrightLines.push(testContents.slice(copyrightMatch.indices[0][0], copyrightMatch.indices[0][1]));
+      testContents = testContents.slice(copyrightMatch.indices[0][1]);
+    }
+    const licenseMatch = licenseLine.exec(testContents);
+    if (licenseMatch) {
+      license = licenseMatch[1];
+      testContents = testContents.slice(licenseMatch.indices[0][1]);
+    }
+    // eat header separator
+    separatorMatch = separatorLine.exec(testContents);
+    if (separatorMatch)
+      testContents = testContents.slice(separatorMatch.indices[0][1]);
+
+    const includes = ['chakracore/adaptor.js'];
+    if (expectedOutput)
+      includes.push('compareArray.js');
+    // Assume the presence of the names of certain auxiliary files mean they are
+    // being imported
+    includes.push(...extraIncludes
+      .filter((include) => testContents.includes(include))
+      .map((include) => `chakracore/${include}`));
+
+    const fd = fs.openSync(path.join(targetDir, testInfo.file), 'w');
+    for (const copyright of copyrightLines)
+      fs.writeSync(fd, copyright);
+    if (license)
+      fs.writeSync(fd, `// SPDX-License-Identifier: ${license}\n`);
+    fs.writeSync(fd, dedent`
+      /*---
+      esid: null
+      description: ChakraCore implementation test ${dir}/${testInfo.file}
+      includes: [${includes.join(', ')}]
+      flags: [noStrict]
+      ---*/
+    `);
+    if (testInfo.wscriptArgs)
+      testInfo.wscriptArgs.forEach((arg) => fs.writeSync(fd, `\nWScript.Arguments.push("${arg}");`));
+    if (includes.includes('chakracore/UnitTestFramework.js'))
+      fs.writeSync(fd, "\nassert = unitTestFrameworkAssert;");
+    fs.writeSync(fd, testContents);
+
+    fs.writeSync(fd, '\nchakraCoreAdaptor.verifyTest(');
+    if (expectedOutput)
+      fs.writeSync(fd, '`' + expectedOutput + '`');
+    fs.writeSync(fd, ');\n');
+  }
+}
